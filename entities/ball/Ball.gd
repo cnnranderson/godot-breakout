fac4731468a250d39f1d3aae41dbe341da63c983
Conner Anderson <cnnranderson@gmail.com>
2021-06-21 19:43:54 -0700

    Cleanup powerup

--- entities/ball/Ball.gd
+++ entities/ball/Ball.gd
@@ -7,6 +7,7 @@ export(int) var speed = 350
 
 onready var width = $Sprite.texture.get_width() * scale.x
 
+var large = false
 var hit_chain = 0
 var velocity = Vector2(0, 1).normalized() * speed
 
@@ -19,14 +20,11 @@ func _physics_process(delta):
 	if collision:
 		var obj = collision.collider
 		
-		# Flip and consume the remaining velocity
-		velocity = velocity.bounce(collision.normal)
-		var reflect = collision.remainder.bounce(collision.normal)
-		move_and_collide(reflect)
-		
 		# Inform objects they were hit
 		match obj.get_meta("type"):
 			"brick":
+				#if not large:
+				_collide_bounce(collision)
 				hit_chain += 1
 				var pitch_shift = min(hit_chain / 2.0, 6) + 1
 				Sounds.play_sound(Sounds.SoundType.SFX, SFX_NORMAL, pitch_shift)
@@ -34,13 +32,21 @@ func _physics_process(delta):
 					obj.hit()
 			"paddle":
 				hit_chain = 0
+				_collide_bounce(collision)
 				velocity = -position.direction_to(obj.position) * speed
 				Sounds.play_sound(Sounds.SoundType.SFX, SFX_NORMAL)
 			_:
+				_collide_bounce(collision)
 				Events.emit_signal("screen_shake")
 				Sounds.play_sound(Sounds.SoundType.SFX, SFX_NORMAL)
 
+func _collide_bounce(collision):
+	velocity = velocity.bounce(collision.normal)
+	var reflect = collision.remainder.bounce(collision.normal)
+	move_and_collide(reflect)
+
 func grow():
+	large = true
 	$Tween.interpolate_property(self, "scale", scale, Vector2.ONE * 4, 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
 	$ScaleTimer.start()
@@ -50,5 +56,6 @@ func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
 
 func _on_ScaleTimer_timeout():
+	large = false
 	$Tween.interpolate_property(self, "scale", scale, Vector2.ONE * 1.5, 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
